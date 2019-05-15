35819832136d9571a0f4880cd81cb8e782e0ca8b
jonpas <jonpas33@gmail.com>
2019-05-15 20:58:00 +0200

    Require speed in DmsPlayer and DmsEnemy

--- dmc/src/DmsObjects/DmsEnemy.cpp
+++ dmc/src/DmsObjects/DmsEnemy.cpp
@@ -11,10 +11,12 @@ bool DmsEnemy::verify() {
     bool found_dmg;
     bool found_hit_chance;
     bool found_hp;
+    bool found_speed;
 
     field_scope.get_field_value<float>("dmg", &found_dmg);
     field_scope.get_field_value<float>("hit_chance", &found_hit_chance);
     field_scope.get_field_value<float>("hp", &found_hp);
+    field_scope.get_field_value<float>("hp", &found_speed);
 
-    return found_dmg && found_hit_chance && found_hp;
+    return found_dmg && found_hit_chance && found_hp && found_speed;
 }
