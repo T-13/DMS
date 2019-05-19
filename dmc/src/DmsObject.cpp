880faac2e734b7ebd198f20846e4e3f4109d12aa
planeer <nejcplaner@gmail.com>
2019-05-19 22:19:24 +0200

    DmsObject copy constructor

--- dmc/src/DmsObject.cpp
+++ dmc/src/DmsObject.cpp
@@ -3,7 +3,8 @@
 DmsObject::DmsObject() {
 }
 
-DmsObject::DmsObject(const DmsObject &/*object*/) {
+DmsObject::DmsObject(const DmsObject &object) {
+    field_scope = object.field_scope;
 }
 
 DmsObject::DmsObject(DmsFieldScope* enclosing_scope) {
